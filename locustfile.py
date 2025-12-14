44d52ae541430b7044841e7eb77b3f8ec4ad49e1
ip314s20 <ip314s20@csc.sibsutis.ru>
2025-12-14 16:38:11 +0700

    test fixed

--- locustfile.py
+++ locustfile.py
@@ -1,28 +1,48 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
+import urllib3
 
-class OpenBNCUser(HttpUser):
-    wait_time = 2
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    @task(3)
-    def get_system_info(self):
+class RedfishUser(HttpUser):
+    """Пользователь для нагрузочного тестирования Redfish API"""
+    
+    host = "https://localhost:2443"
+    wait_time = between(1, 3)
+    
+    def on_start(self):
+        """Инициализация пользователя при начале теста"""
+        self.credentials = ("root", "0penBmc")
+    
+    @task
+    def fetch_system_information(self):
+        """Получение общей информации о системе"""
         self.client.get(
-            "/redfish/v1/systems/system",
-            auth=("root", "OpenBmc"),
-            verify=False
+            "/redfish/v1/Systems/system",
+            auth=self.credentials,
+            verify=False,
+            name="01_System_Information"
         )
-
-    @task(2)
-    def get_power_state(self):
-        self.client.get(
-            "/redfish/v1/Chassis/chassis",
-            auth=("root", "OpenBmc"),
-            verify=False
-        )
-
-    @task(1)
-    def get_service_root(self):
-        self.client.get(
-            "/redfish/v1/",
-            auth=("root", "OpenBmc"),
-            verify=False
-        )
+    
+    @task
+    def verify_power_state(self):
+        """Проверка текущего состояния питания системы"""
+        with self.client.get(
+            "/redfish/v1/Systems/system",
+            auth=self.credentials,
+            verify=False,
+            name="02_Power_State_Verification",
+            catch_response=True
+        ) as response:
+            if response.status_code == 200:
+                try:
+                    power_state = response.json().get("PowerState")
+                    valid_power_states = {"On", "Off", "PoweringOn", "PoweringOff"}
+                    
+                    if power_state in valid_power_states:
+                        response.success()
+                    else:
+                        response.failure(f"Недопустимое состояние питания: {power_state}")
+                except ValueError:
+                    response.failure("Некорректный JSON-ответ")
+            else:
+                response.failure(f"Ошибка HTTP: {response.status_code}")
